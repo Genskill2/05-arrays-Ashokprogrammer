762941a7b3067bc7145c6458e12571d592600624
Ashokprogrammer <86102328+Ashokprogrammer@users.noreply.github.com>
2021-06-19 20:38:20 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,2 +1,19 @@
-/* Enter your solutions in this file */
 #include <stdio.h>
+#include <cs50.h>
+int main() {
+int n;
+int max[n];
+n=get_int("Enter the value of n:\n");
+for(int i=0;i<n;i++){
+max[¡]=get_int("Enter number%i:");
+}
+int maximum=max[0];
+for(int i=0;i<n;i++){
+if(max[i]>maximum){
+maximum=max[i];
+}
+}
+Printf("maximum value is %i\n",maximum);
+}
+
+
